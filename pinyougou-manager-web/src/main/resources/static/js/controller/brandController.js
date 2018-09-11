cde8eeaee45c8d587d896fa45314967ad8e27add
Yunfeng-Jiang <frankjiang1994@gmail.com>
2018-09-11 22:22:48 +0800

    JS分离抽取

---
+++ pinyougou-manager-web/src/main/resources/static/js/controller/brandController.js
@@ -0,0 +1,73 @@
+app.controller('brandController',function($scope,$controller,brandService){
+    // 继承父类js
+    $controller('baseController',{$scope:$scope});
+
+    //查询品牌列表
+    $scope.findAll=function(){
+        brandService.findAll().success(
+            function(response){
+                $scope.list=response;
+            }
+        );
+    };
+    //分页
+    $scope.findPage=function(page,size){
+        brandService.findPage(page,size).success(
+            function(response){
+                $scope.list=response.rows;//显示当前页数据
+                $scope.paginationConf.totalItems=response.total;//更新总记录数
+            }
+        );
+    };
+    // 新增
+    $scope.save = function () {
+        var object = null;
+        if ($scope.entity.id != null) {
+            object = brandService.update($scope.entity);
+        } else {
+            object = brandService.add($scope.entity);
+        }
+        object.success(
+            function (response) {
+                if (response.success) {
+                    // 刷新
+                    $scope.reloadList();
+                } else {
+                    alert(response.message);
+                }
+            }
+        );
+    };
+    // 查询实体
+    $scope.findOne = function (id) {
+        brandService.findOne(id).success(
+            function (response) {
+                $scope.entity = response;
+            }
+        );
+    };
+    //删除
+    $scope.dele=function(){
+        if(confirm('确定要删除吗？')){
+            brandService.dele($scope.selectIds).success(
+                function(response){
+                    if(response.success){
+                        $scope.reloadList();//刷新
+                    }else{
+                        alert(response.message);
+                    }
+                }
+            );
+        }
+    };
+    $scope.searchEntity = {};
+    //条件查询
+    $scope.search = function(page,size){
+        brandService.search(page, size, $scope.searchEntity).success(
+            function(response){
+                $scope.list=response.rows;//显示当前页数据
+                $scope.paginationConf.totalItems=response.total;//更新总记录数
+            }
+        );
+    }
+});
---
+++ pinyougou-manager-web/src/main/resources/static/js/service/brandService.js
@@ -0,0 +1,31 @@
+// 品牌服务
+app.service("brandService", function ($http) {
+    this.findAll = function () {
+        return $http.get('../brand/findAll');
+    };
+
+    this.findPage = function (page,size) {
+        return $http.get('../brand/findPage?page='+page +'&size='+size);
+    };
+
+    this.findOne = function (id) {
+        return $http.get('../brand/findOne?id=' + id);
+    };
+
+    this.add = function (entity) {
+        return $http.post('../brand/add', entity);
+    };
+
+    this.update = function (entity) {
+        return $http.post('../brand/update', entity);
+    };
+
+    this.dele = function (ids) {
+        return $http.get('../brand/delete?ids=' + ids);
+    };
+
+    this.search = function (page, size, searchEntity) {
+        return $http.post('../brand/search?page=' + page +'&size=' + size, searchEntity);
+    };
+
+});
